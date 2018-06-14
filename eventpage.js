bb49a158a484fd669f7cce13a9ce27995ea8d1ff
Subhroneel Dey <subhroneeldey4@outlook.com>
2018-06-14 19:11:41 +0530

    context Menu listener added

--- eventpage.js
+++ eventpage.js
@@ -5,3 +5,11 @@ var contextMenuItem={
 
 };
 chrome.contextMenus.create(contextMenuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickPosition){
+    if(clickPosition.menuItemId=="specialBookmark" && clickPosition.selectionText)
+    {
+        
+    }
+
+})
