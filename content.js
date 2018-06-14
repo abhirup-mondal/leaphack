e1b12b69bd5559a5d73911b4f333bcf97f42b893
Abhirup Mondal <abhirup.git@gmail.com>
2018-06-14 11:33:43 +0530

    event handler working

--- content.js
+++ content.js
@@ -1,14 +1,23 @@
-var saveButton = document.createElement("button");
-var saveText = document.createTextNode("Save")
+var saveButton = document.createElement('button');
+var saveText = document.createTextNode('Save');
 saveButton.appendChild(saveText);
 
+saveButton.setAttribute('class', 'saveButton');
 
 // var removeButton = document.createElement("button");
 // removeButton.innerHTML("remove");
 
-var body = document.getElementsByClassName("rc");
+googleNodeClass = 'rc';
+var body = document.getElementsByClassName(googleNodeClass);
+
 for (var i = 0; i < body.length; ++i) {
     var childNode = saveButton.cloneNode(true);
+    childNode.addEventListener('click', saveBtnHandler);
+    hyperlink = body[i].childNodes[0].childNodes[0].getAttribute('href');
+    childNode.setAttribute('id', hyperlink);
     body[i].appendChild(childNode);
-    console.log(body[i].childNodes[0].childNodes[0].getAttribute('href'));
+}
+
+function saveBtnHandler(e) {
+    console.log(e.target.getAttribute('id'));
 }
